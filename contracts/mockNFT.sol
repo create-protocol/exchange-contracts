7eecba6661ef5835563056714fa03de264b639e6
smrpn <samarpan_d@pp.iitr.ac.in>
2022-03-28 15:57:31 +0530

    ft: remove yarn.lock
    Signed-off-by: smrpn <samarpan_d@pp.iitr.ac.in>

--- contracts/mockNFT.sol
+++ contracts/mockNFT.sol
@@ -2,19 +2,20 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract mockNft is ERC721, Ownable {
+contract mockNFT is ERC721, Ownable {
     uint256 public _tokenIds;
     constructor() ERC721("MockNFT", "MNFT") Ownable(){
         _tokenIds = 0;
     }
 
     function mint() public {
-        _tokenIds++;
+        ++_tokenIds;
         _safeMint(msg.sender, _tokenIds);
     }
 
-    function mintHundred() public {
-        for(uint i = 0; i<100; i++) {
+    function mintHundred(uint32 n) public {
+        for(uint i = 0; i<n; i++) {
+            ++_tokenIds;
             _safeMint(msg.sender, _tokenIds);
         }
     }
